3af72a3fafaba5b0aa854436ae19dcd2ae9e718d
Samuel Path <samuel.path@gmail.com>
2022-05-23 17:18:06 +0200

    remove contract initial filtering

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 function loadTable() {
     var table = new Tabulator("#example-table", {
-        data: tabledata, //assign data to table
-        columns:[          //define the table columns
-          { title: "Entreprise", field: "Entreprise" },
-          { title: "Titre", field: "Titre" },
-          { title: "Années XP", field: "Années XP", sorter: "number" },
-          { title: "Brut/CA (k€)", field: "Brut/CA (k€)", sorter: "number"},
-          { title: "Contrat", field: "Contrat", headerFilter: true },
-          { title: "Package (k€)", field: "Package (k€)", sorter: "number"  },
-          { title: "Détails package", field: "Détails package" },
-          { title: "Remarques libres", field: "Remarques libres" }
-        ],
-        initialSort:[ //set the initial sort order of the data
-          {column: "Brut/CA (k€)", dir:"desc"},
-        ],
-        initialFilter:[
-          {field:"Contrat", type:"=", value:"CDI"}
+      data: tabledata, //assign data to table
+      columns:[          //define the table columns
+        { title: "Entreprise", field: "Entreprise" },
+        { title: "Titre", field: "Titre" },
+        { title: "Années XP", field: "Années XP", sorter: "number" },
+        { title: "Brut/CA (k€)", field: "Brut/CA (k€)", sorter: "number"},
+        { title: "Contrat", field: "Contrat", headerFilter: true },
+        { title: "Package (k€)", field: "Package (k€)", sorter: "number"  },
+        { title: "Détails package", field: "Détails package" },
+        { title: "Remarques libres", field: "Remarques libres" }
       ],
+      initialSort:[ //set the initial sort order of the data
+        {column: "Brut/CA (k€)", dir:"desc"},
+      ],
+      // initialFilter:[
+      //   {field:"Contrat", type:"=", value:"CDI"}
+      // ]
     });
 };
 
